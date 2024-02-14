8e36412318eb05bdc5ce53251274db04e89b0cc9
Matt Ihnen <mihnen@milwaukeeelectronics.com>
2024-02-14 11:19:13 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,4 @@
+
+if vim.fn.has("gui_running") then
+  vim.opt.guifont = "JetBrains Mono"
+end
---
+++ plugins.lua
@@ -0,0 +1,64 @@
+local plugins = {
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require "custom.configs.null-ls"
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require "plugins.configs.lspconfig"
+			require "custom.configs.lspconfig"
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"clang-format",
+			}
+		}
+	},
+	{
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        -- "vue", "svelte",
+
+       -- low level
+        "c",
+        "cpp",
+        "rust",
+        "zig",
+        "go",
+
+        "python",
+        "markdown",
+        "markdown_inline",
+        "c_sharp",
+        "doxygen",
+        "latex",
+        "rst",
+        "yaml",
+      },
+      indent = {
+      	enable = true
+      },
+    },
+  }
+}
+return plugins
